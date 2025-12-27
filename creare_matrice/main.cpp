7528a29a50f69850569cb0a92e3657cc4e81332d
MarkTwain08 <markdanielbanc@gmail.com>
2025-12-27 14:28:35 +0200

    plssssss

--- creare_matrice/main.cpp
+++ creare_matrice/main.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+
 int a[101][101];
 int verifica[200];
+
 int main()
 {
     FILE *f = fopen("text_test.txt", "r");
@@ -13,17 +16,17 @@ int main()
     char target2[] = "Value";
     if (!f)
     {
+        cerr << "Error: text_test.txt not found\n";
         return 1;
     }
+
     int i=0,sum=1,j=1,maxim=-1,maximi=-1,ok=0,k;
     while (fgets(line, sizeof(line), f))
     {
-        //pentru cuvantul breakpoint
         if (strstr(line, target1))
         {
             ok=1;
-            if(maximi<i)
-                maximi=i;
+            if(maximi<i) maximi=i;
             verifica[i+1]=1;
             if(a[i+1][1]!=0)
             {
@@ -31,23 +34,19 @@ int main()
                     if(a[i+1][j]==0)
                     {
                         a[i+1][j]+=1;
-                        if(maxim<j)
-                            maxim=j;
+                        if(maxim<j) maxim=j;
                         j=101;
                     }
             }
             else
             {
                 a[i+1][1]+=1;
-                if(maxim<1)
-                    maxim=1;
+                if(maxim<1) maxim=1;
             }
             i++;
         }
-        //pentru cuvantul value
         else if (strstr(line, target2))
         {
-            //pentru valorile care is la capatul crengii
             if(ok==1)
                 for(j=2; j<=100; j++)
                 {
@@ -58,8 +57,7 @@ int main()
                     }
                 }
             ok=0;
-            if(maximi<i)
-                maximi=i;
+            if(maximi<i) maximi=i;
             i--;
             if(verifica[i]==1)
                 verifica[i]=0;
@@ -78,7 +76,7 @@ int main()
     }
 
     fclose(f);
-    // punerea lui -1 in locuri care necesita spatiu
+
     for(i=1; i<maximi; i++)
     {
         j=1;
@@ -91,34 +89,31 @@ int main()
             if(a[i][j]==9 || a[i][j]==-1)
             {
                 k=1;
-                while(a[i+1][k]!=0)
-                    k++;
-                if(k>maxim)
-                    maxim=k;
-                for(j=k+1; j>sum; j--)
-                    a[i+1][j]=a[i+1][j-1];
+                while(a[i+1][k]!=0) k++;
+                if(k>maxim) maxim=k;
+                for(j=k+1; j>sum; j--) a[i+1][j]=a[i+1][j-1];
                 a[i+1][sum]=-1;
             }
             j++;
         }
     }
-    //afisare
+    // --- Write matrix to text file with dimensions ---
+    ofstream out("matrix.txt");
+    out << maximi <<" "<< maxim << "\n\n";
 
     for(i=1; i<=maximi; i++)
     {
         for(j=1; j<=maxim; j++)
         {
-            if(a[i][j]==0)
-                cout<<" ";
-            else
-            {
-                if(a[i][j]!=-1)
-                    cout<<a[i][j];
-                else cout<<" ";
-            }
-        }
-        cout<<endl;
-    }
+            int val = a[i][j];
+            out << val;
+            if (j < maxim) out << " "; // space only between numbers
 
+        }
+        out << endl;
+    }
+    out.close();
+
+    cout << "Matrix saved to matrix.txt\n";
     return 0;
 }
