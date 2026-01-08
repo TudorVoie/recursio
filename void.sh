50fb229fe1465efd60fa39b506d15f5694830f02
Tudor Voie <tudor_voie@yahoo.com>
2026-01-08 09:28:12 +0200

    reparat o chestiuta

--- void.sh
+++ void.sh
@@ -33,4 +33,4 @@ timeout 3s gdb -batch -x stiva.gdb a.out > gdb.txt
 #grep -E "Value returned is" gdb.txt
 #grep -E "Breakpoint [0-9]+,|Value returned is" gdb.txt
 #grep -oP '.*(?=\[TUDOR\]$)' gdb.txt
-grep -E "Breakpoint [0-9]+,|Value returned is|\[TUDOR\]$" gdb.txt | sed 's/\[TUDOR\]$//'
+grep -E "Breakpoint [0-9]+,|Value returned is|input.cpp|\[TUDOR\]$" gdb.txt | sed 's/\[TUDOR\]$//' | head -n -1
