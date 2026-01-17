d4bac614cbfa21b1f179ee96c03b55e3b8addf1f
Tudor Voie <tudor_voie@yahoo.com>
2026-01-17 20:31:54 +0200

    facem primu inceput la e

--- int.sh
+++ int.sh
@@ -16,7 +16,7 @@ cat template_stiva.gdb >> stiva.gdb
 
 # fara optimizari
 g++ -O0 -g -fno-inline -fno-omit-frame-pointer input.cpp -o a.out
-timeout 3s gdb -batch -x stiva.gdb a.out > gdb.txt
+timeout 1s gdb -batch -x stiva.gdb a.out > gdb.txt
 #grep -E "Breakpoint [0-9]+," gdb.txt
 #grep -E "Value returned is" gdb.txt
 grep -E "Breakpoint [0-9]+,|Value returned is" gdb.txt
